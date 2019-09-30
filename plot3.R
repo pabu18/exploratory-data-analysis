4baa62d7e0c5d35ed78546de8c687dd4812a3f47
Ansh Mehta <30555898+anshmehtamm@users.noreply.github.com>
2019-10-01 02:10:08 +0530

    Changed date as range was incorrect
    changed date to 2007-09-31

--- plot3.R
+++ plot3.R
@@ -1,6 +1,6 @@
 y<-read.csv("household_power_consumption.txt",sep=";",stringAsFactorS=FALSE)
 y[,1]<-as.Date(y[,1],"%d/%m/%Y")
-t<-y[,1]>="2007-02-01"&y[,1]<="2007-02-02"
+t<-y[,1]>="2007-02-01"&y[,1]<="2007-09-31"
 y<-y[t,]
 datetime<-paste(y[,1],y[,2])
 y[,2]<-as.POSIXct(datetime)
