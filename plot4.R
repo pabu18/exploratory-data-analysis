6f694e8f33d503b43660a6a98553215e493d2b8b
Ansh Mehta <30555898+anshmehtamm@users.noreply.github.com>
2019-10-01 02:13:35 +0530

    Changed plot4.png to plot3.png

--- plot4.R
+++ plot4.R
@@ -4,7 +4,7 @@ t<-y[,1]>="2007-02-01"&y[,1]<="2007-02-02"
 y<-y[t,]
 datetime<-paste(y[,1],y[,2])
 y[,2]<-as.POSIXct(datetime)
-png("plot4.png",width=480,height=480)
+png("plot3.png",width=480,height=480)
 par(mfrow=c(2,2))
 with(y,plot(Global_active_power~Time,type="l",ylab="Global Active Power",xlab=""))
 with(y,plot(Voltage~Time,type="l",ylab="Voltage",xlab="datetime"))
