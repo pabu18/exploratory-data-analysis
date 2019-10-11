25491eea51053a358f61284e13a91bcd822b9377
ritik1999 <33102844+ritik1999@users.noreply.github.com>
2019-10-11 22:19:08 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -2,6 +2,7 @@ y<-read.csv("household_power_consumption.txt",sep=";",stringAsFactorS=FALSE)
 y[,1]<-as.Date(y[,1],"%d/%m/%Y")
 t<-y[,1]>="2007-02-01"&y[,1]<="2007-02-02"
 y<-y[t,]
+##you should also see it in scater plot for detailed analysis.
 png("plot1.png",width=480,height=480)
 hist(y$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
